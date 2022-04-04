fee9a495dd196ae182179b2ea1a7944854a1d0f5
Harikrishna Pallikere <42885238+haripallikere@users.noreply.github.com>
2022-04-05 01:08:09 +0530

    infinite-slider-working

--- main.js
+++ main.js
@@ -1,6 +1,94 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// images
+const images = [
+  "images/image5.jpg",
+  "images/image1.jpg",
+  "images/image2.jpg",
+  "images/image3.jpg",
+  "images/image4.jpg",
+  "images/image5.jpg",
+  "images/image1.jpg",
+];
+
+// selectors
+const slides = document.querySelector(".slide");
+const nextBtn = document.querySelector(".slider__btn--right");
+const previousBtn = document.querySelector(".slider__btn--left");
+
+// class declaration
+class ImageSlider {
+  constructor(slide, images, document) {
+    this.slide = slide;
+    this.images = images;
+    this.slideIndex = 1;
+    this.document = document;
+    this.isTransition = false;
+  }
+
+  //populates images to dom
+  generateImages = () => {
+    this.slide.innerHTML = this.images
+      .map((v) => {
+        return `<img src="${v}">`;
+      })
+      .join("");
+  };
+
+  // move images
+  moveSlide = () => {
+    // if (this.isTransition) {
+    //   return;
+    // }
+    this.slide.style.transform = `translateX(-${this.slideIndex * 100}%)`;
+  };
+
+  // onclick
+  moveHandler(direction) {
+    this.isTransition = true;
+    this.slide.style.transition = "transform .6s ease-in-out";
+    if (direction === "right") {
+      this.slideIndex += 1;
+      this.moveSlide();
+    } else if (direction === "left") {
+      this.slideIndex -= 1;
+      this.moveSlide();
+    }
+  }
+
+  //infinite-slider
+  inifiniteSlide() {
+    this.isTransition = false;
+    if (this.slideIndex === 0) {
+      this.slide.style.transition = "none";
+      this.slideIndex = this.images.length - 2;
+      this.moveSlide();
+    } else if (this.slideIndex === this.images.length - 1) {
+      this.slide.style.transition = "none";
+      this.slideIndex = 1;
+      this.moveSlide();
+    }
+  }
+}
+
+// instance
+const slider = new ImageSlider(slides, images, document);
+
+// initialisation
+slider.generateImages();
+slider.moveSlide();
+
+//event listers handlers;
+nextBtn.addEventListener("click", () => {
+  if (slider.isTransition) return;
+  slider.moveHandler("right");
+});
+
+previousBtn.addEventListener("click", () => {
+  if (slider.isTransition) return;
+  slider.moveHandler("left");
+});
+
+slides.addEventListener("transitionend", () => {
+  slider.inifiniteSlide();
+});
