7680f448c2a3bcbf2ee785c6ab73d2f0235a7fdd
Harikrishna Pallikere <42885238+haripallikere@users.noreply.github.com>
2022-04-05 10:03:28 +0530

    final version

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-// images
+// images links
 const images = [
   "images/image5.jpg",
   "images/image1.jpg",
@@ -15,15 +15,17 @@ const images = [
 const slides = document.querySelector(".slide");
 const nextBtn = document.querySelector(".slider__btn--right");
 const previousBtn = document.querySelector(".slider__btn--left");
+const navigationBar = document.querySelector(".navigation__bar");
 
 // class declaration
 class ImageSlider {
-  constructor(slide, images, document) {
+  constructor(slide, images, document, indicatorsContainer) {
     this.slide = slide;
     this.images = images;
     this.slideIndex = 1;
     this.document = document;
     this.isTransition = false;
+    this.indicatorsContainer = indicatorsContainer;
   }
 
   //populates images to dom
@@ -35,11 +37,23 @@ class ImageSlider {
       .join("");
   };
 
+  //populate indicators
+  generateIndicators() {
+    const removeClonedImages = [...this.images];
+    removeClonedImages.shift();
+    removeClonedImages.pop();
+
+    this.indicatorsContainer.innerHTML = removeClonedImages
+      .map((v, index) => {
+        return `<button class="navigation__indicators" id="${index}"></button>`;
+      })
+      .join("");
+    // return a;
+  }
+
   // move images
   moveSlide = () => {
-    // if (this.isTransition) {
-    //   return;
-    // }
+    console.log(this.slideIndex);
     this.slide.style.transform = `translateX(-${this.slideIndex * 100}%)`;
   };
 
@@ -54,6 +68,7 @@ class ImageSlider {
       this.slideIndex -= 1;
       this.moveSlide();
     }
+    this.showActiveIndicator();
   }
 
   //infinite-slider
@@ -69,14 +84,44 @@ class ImageSlider {
       this.moveSlide();
     }
   }
+
+  indicator(e) {
+    let id = Number(e.target.id);
+    this.slideIndex = id + 1;
+    this.slide.style.transition = "transform .6s ease-in-out";
+    this.moveSlide();
+    this.showActiveIndicator();
+  }
+
+  showActiveIndicator() {
+    const indicators = this.document.querySelectorAll(
+      ".navigation__indicators"
+    );
+    indicators.forEach((v) => {
+      v.classList.remove("active");
+    });
+    switch (this.slideIndex) {
+      case 0:
+        indicators[indicators.length - 1].classList.add("active");
+        break;
+      case this.images.length - 1:
+        indicators[0].classList.add("active");
+        break;
+      default:
+        indicators[this.slideIndex - 1].classList.add("active");
+        break;
+    }
+  }
 }
 
 // instance
-const slider = new ImageSlider(slides, images, document);
+const slider = new ImageSlider(slides, images, document, navigationBar);
 
 // initialisation
 slider.generateImages();
 slider.moveSlide();
+slider.generateIndicators();
+slider.showActiveIndicator();
 
 //event listers handlers;
 nextBtn.addEventListener("click", () => {
@@ -91,4 +136,13 @@ previousBtn.addEventListener("click", () => {
 
 slides.addEventListener("transitionend", () => {
   slider.inifiniteSlide();
+});
+
+// console.log(indicators, "d");
+
+const indicators = document.querySelectorAll(".navigation__indicators");
+indicators.forEach((v) => {
+  v.addEventListener("click", (e) => {
+    slider.indicator(e);
+  });
 });
